f160615b0015b20a6d345e001b55ce9b43cb18bc
Nate Robinson <nrobinson@qslwm.com>
2015-12-19 08:24:04 -0600

    Begin configuring template parts for testimonial archive page

--- wp-content/themes/willowick/archive-testimonial.php
+++ wp-content/themes/willowick/archive-testimonial.php
@@ -30,12 +30,11 @@ get_header(); ?>
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				 */;
+				get_template_part( 'template-parts/content', 'testimonial' );
 
 			endwhile;
-
-			the_posts_navigation();
+                        
 
 		else :
 
--- wp-content/themes/willowick/template-parts/content-testimonial.php
+++ wp-content/themes/willowick/template-parts/content-testimonial.php
@@ -10,35 +10,18 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php willowick_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+            
+            <blockquote>
+            
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'willowick' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'willowick' ),
-				'after'  => '</div>',
-			) );
+                
+                    the_content();
+                    the_title("<author>", "</author>");
+                    
 		?>
+            </blockquote>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
